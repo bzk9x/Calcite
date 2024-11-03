5e98b043c92cbc1a3a04fecd6dfc1725bcf373b2
bzk9x <168664574+bzk9x@users.noreply.github.com>
2024-11-04 00:24:49 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    transparent: true,
+    frame: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  win.maximize();
+  win.loadFile('app/index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+console.log("bzk9x");
